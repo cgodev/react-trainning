c57b537a8157e09ee4159d9846bd683fd4af9b3a
Carlos Humberto Gomez Restrepo <carloshumberto.gomez@globant.com>
2022-03-23 20:33:22 -0500

    useEffect Done

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
 
@@ -11,13 +11,13 @@ const Accordion = ({ items }) => {
 
     const renderedItems = items.map((item, index) => {
 
-        const active = index === activeIndex ? 'active': ''
+        const active = index === activeIndex ? 'active' : ''
 
         return (
             <React.Fragment key={item.title}>
                 <div
                     className={`title ${active}`}
-                    onClick={ () => onTitleClick(index) }
+                    onClick={() => onTitleClick(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
--- widgets/src/components/WikiSearch.js
+++ widgets/src/components/WikiSearch.js
@@ -1,19 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const WikiSearch = () => {
 
-    const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const wikiSearch = async () => {
+      const { data } = await axios.get(
+        'https://en.wikipedia.org/w/api.php',
+        {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term
+          }
+        });
+      setResults(data.query.search);
+    }
+
+    if (term && !results.length) {
+      wikiSearch();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) wikiSearch();
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      }
+    }
+
+  }, [term]);
+
+  const renderedResults = results.map(result => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Search whatever u want</label>
-                    <input onChange={e => setTerm(e.target.value)} value={term} className="input"/>
-                </div>
-            </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`} target="_blank">See more</a>
         </div>
-    )
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Search whatever u want</label>
+          <input onChange={e => setTerm(e.target.value)} value={term} className="input" />
+        </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
+      </div>
+    </div>
+  )
 }
 
 export default WikiSearch;
